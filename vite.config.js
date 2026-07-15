9ceb81a09394378e621c0d954c48814600fa3d2a
Zarkasih <zarkasih081@users.noreply.github.com>
2026-07-15 21:00:49 +0700

    chore: add gh-pages deploy script and configure base URL

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
+  base: '/spp-bendahara/',
   plugins: [viteSingleFile()],
   build: {
     assetsInlineLimit: 100000000,
