8584825d370eef57d9571403781a4242e8bebc00
Zarkasih <zarkasih081@users.noreply.github.com>
2026-07-15 21:24:07 +0700

    fix: bundle application into root index.html to fix blank page on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/spp-bendahara/',
+  root: 'src',
+  base: './',
   plugins: [viteSingleFile()],
   build: {
+    outDir: '../',
+    emptyOutDir: false,
     assetsInlineLimit: 100000000,
   }
 });
